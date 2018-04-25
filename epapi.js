981b0b0db3e07208dafb2499e5e6c770adb5a237
Astra <dr1ft.ast@gmail.com>
2018-04-24 22:50:03 -0400

    make it easier to check for lite mode

--- epapi.js
+++ epapi.js
@@ -299,7 +299,7 @@ exports = {
 
         major: 5,
         minor: 3,
-        revision: 27,   // TODO:    find a better way of incrementing/calculating the revision; the current way is fucking ridiculous (manually editing)
+        revision: 28,   // TODO:    find a better way of incrementing/calculating the revision; the current way is fucking ridiculous (manually editing)
 
         toString: function () {
             return `v${this.major}.${this.minor}.${this.revision}`;
@@ -348,7 +348,7 @@ exports = {
             __print('starting up...')
 
             // dont use node integration
-            __lite = lite ? 1 : 0;
+            exports.lite = __lite = lite ? true : false;
 
             // prepare the global namespace
             __print('preparing the global namespace...');
